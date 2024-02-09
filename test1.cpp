b2acf0c5193435425689518ba31db35ad07ff39e
karenaprince <159529658+karenaprince@users.noreply.github.com>
2024-02-09 15:35:36 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int a,b;
+
+    cout <<"Enter the value of a :";
+    cin >>a;
+    cout <<"Enter the value of b : ";
+    cin >>b;
+
+    cout <<"add = " <<a+b <<endl;
+    cout <<"multiplication = " <<a*b <<endl;
+    cout <<"modular = " <<a%b <<endl;
+    cout <<"division = " <<a/b <<endl;
+    cout <<"suprection = " <<a-b <<endl;
+
+    return 0;
+}
